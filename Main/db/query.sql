b66839dd4a67afda4e988f9544ec5ea8cbdc8934
Five Pandas <amandapaul1223@gmail.com>
2023-03-09 17:57:55 -0700

    seeking assistance on sql

---
+++ Main/db/query.sql
@@ -0,0 +1,9 @@
+SELECT employee.first_name AS employee first name, 
+employee.last_name AS employee last name, employee.job_title AS job title, 
+employee.department AS department, employee.salary AS salary, 
+employee.manager AS manager
+FROM Employee
+LEFT JOIN Role
+ON employee.role_id = role.title;
+
+
---
+++ Main/db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO Role (title, department_id, salary)
+VALUES ("Intern", 1, 10000),
+       ("Customer Service Rep", 2, 20000),
+       ("Tech Support", 3, 30000),
+       ("Department Supervisor", 4, 40000),
+       ("HR Rep", 5, 5000);
+
+INSERT INTO Department (department_name)
+VALUES ("IT"),
+       ("Customer Relations"),
+       ("Human Resources"),
+       ("Management")
+
+INSERT INTO Manager ()
+
+INSERT INTO Employee (first_name, last_name, Role, Department, salary, manager)
+VALUES ("Hepworth","Kimberly", 1, 1, )
